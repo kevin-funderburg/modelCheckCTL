d1cf9b8b296dcb79d75aa11af26df345573dd863
kevin-funderburg <ksfunderburg@icloud.com>
2021-11-23 12:26:30 -0600

    updated transition class

--- src/modelCheckCTL/util/Transition.java
+++ src/modelCheckCTL/util/Transition.java
@@ -1,4 +1,29 @@
 package modelCheckCTL.util;
 
 public class Transition {
+
+    String name;
+    State FromState;
+    State ToState;
+
+    public Transition(State fromState, State toState)
+    {
+        name = "";
+        FromState = fromState;
+        ToState = toState;
+    }
+
+
+    public Transition(String TransitionName, State fromState, State toState)
+    {
+        name = TransitionName;
+        FromState = fromState;
+        ToState = toState;
+    }
+
+
+    public boolean isEqual(Transition other)
+    {
+        return FromState.isEqual(other.FromState) && ToState.isEqual(other.ToState);
+    }
 }
