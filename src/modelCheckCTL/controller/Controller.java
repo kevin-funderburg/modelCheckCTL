c99979366ff850eaf6ccbad4bd0ea35be385497f
kevin-funderburg <ksfunderburg@icloud.com>
2021-11-28 16:45:25 -0600

    added filename check

--- src/modelCheckCTL/controller/Controller.java
+++ src/modelCheckCTL/controller/Controller.java
@@ -23,21 +23,36 @@ public class Controller {
 		BufferedReader reader = new BufferedReader(
 				new InputStreamReader(System.in));
 
-		System.out.println("Welcome to our Model Check CTL!\n\n" +
-				"Available Kripke structures: 1, 2, 3, 4, 5, 6, 7, Microwave");
+		String[] filenames = {"1", "2", "3", "4", "5", "6", "7", "Microwave"};
+		System.out.println("Welcome to our Model Check CTL!\n\n");
+		System.out.print("Available Kripke structures: ");
+		for (String fn : filenames)
+			System.out.print(fn + " ");
+
+		System.out.print("\n");
 
 		System.out.print("Enter the desired model: ");
 		String model = reader.readLine();
 
-		System.out.print("Enter the CTL formula: ");
-		String ctlExpression = reader.readLine();
-		System.out.println("you entered: " + ctlExpression);
+		boolean validName = false;
+		for (String fn : filenames)
+		{
+			if (fn.equals(model))
+				validName = true;
+		}
+
+		if (!validName)
+			throw new Exception("invalid file name");
 
 		String filename;
 		if (model.equals("Microwave"))
-			 filename = "Microwave Model";
+			filename = "Microwave Model";
 		else
 			filename = "Model " + model;
+
+		System.out.print("Enter the CTL formula: ");
+		String ctlExpression = reader.readLine();
+		System.out.println("you entered: " + ctlExpression);
 
 		Path path = Path.of("Test Files/" + filename  + ".txt");
 		String kripkeDef = Files.readString(path);
